b2b8684be2c0ab6e5f4e8d1974b0ebc0e1cffa14
WilliamSalcedo <wsl_1994@hotmail.com>
2025-12-29 15:47:37 -0500

    Feature implement logic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
- 
-
   return (
-    <>
-    <h1>Hola Be Kind</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
