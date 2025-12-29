36680d7c7b85cf158733321074cabeb6a6c9e81f
WilliamSalcedo <wsl_1994@hotmail.com>
2025-12-29 16:57:42 -0500

    update logic router in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-
+import { PublicRoute } from "./routes/PublicRoute";
+import { PrivateRoute } from "./routes/PrivateRoute";
+import { useAuthStore } from "./context/authStore";
 
 function App() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+console.log(isAuthenticated,"hola")
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/dashboard" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
