25a244966ec3361a131a3bda04a4494e7b61cb34
WilliamSalcedo <wsl_1994@hotmail.com>
2025-12-30 00:59:09 -0500

    feature test home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,43 @@
-import { Link } from "react-router-dom";
+import { DashboardLayout } from "../components/DashboardLayout"
 
 function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <Link to="/login">Ir a Login</Link>
-      <Link to="/dashboard">Ir a Dashboard</Link>
-    </>
-  );
+    <DashboardLayout>
+      <div
+        className="
+          relative
+          flex
+          min-h-[calc(100vh-56px)]
+          items-center
+          justify-center
+          bg-cover
+          bg-center
+        "
+        style={{
+          backgroundImage: "url('/Background.png')",
+        }}
+      >
+        <div className="absolute inset-0 bg-white/80" />
+
+        <div className="relative z-10 flex flex-col items-center text-center px-6">
+          <img
+            src="/Logo.png"
+            alt="Be Kind Network"
+            className="mb-6 w-40 md:w-52"
+          />
+
+          <h1 className="text-2xl md:text-3xl font-semibold text-gray-900 mb-2">
+            ¡Bienvenido a Be Kind Network!
+          </h1>
+
+          <p className="text-gray-600 max-w-md">
+            Desde aquí podrás gestionar las acciones, visualizar el impacto
+            social y administrar la comunidad.
+          </p>
+        </div>
+      </div>
+    </DashboardLayout>
+  )
 }
 
-export default Home;
+export default Home
