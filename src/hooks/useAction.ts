621887542c81b6ce23c0e02475c77c355043d549
WilliamSalcedo <wsl_1994@hotmail.com>
2025-12-30 00:58:02 -0500

    fix logic for the reload actions

--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -1,52 +1,57 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import { useAuthStore } from "../context/authStore"
 import type { Action } from "../types/action"
 import { fetchAction } from "../api/action"
 
 export const useActions = () => {
-  const token = useAuthStore((state) => state.accesToken)
+  const token = useAuthStore((state) => state.accesToken) 
+
   const [actions, setActions] = useState<Action[]>([])
   const [pageNumber, setPageNumber] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
+
   const pageSize = 10
 
-  useEffect(() => {
+  const loadActions = useCallback(async () => {
     if (!token) return
-    const loadActions = async () => {
-      setIsLoading(true)
-      setError(null)
-      try {
-        const data = await fetchAction(token, pageNumber, pageSize)
-        console.log(data, "este es la data de las acciones")
-        setActions(data.data)
-        setTotalPages(data.totalPages)
-      } catch (error) {
-        setError("No se pudieron cargar las acciones")
-        console.log(error, "error de las acciones")
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    loadActions()
 
-  }, [pageNumber, token])
+    setIsLoading(true)
+    setError(null)
+
+    try {
+      const data = await fetchAction(token, pageNumber, pageSize)
+      setActions(data.data)
+      setTotalPages(data.totalPages)
+    } catch (err) {
+      setError("No se pudieron cargar las acciones")
+      console.error(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [token, pageNumber])
+
+  useEffect(() => {
+    loadActions()
+  }, [loadActions])
 
   const nextPage = () => {
     setPageNumber((current) => Math.min(current + 1, totalPages))
   }
 
   const prevPage = () => {
-    setPageNumber((current) => Math.max(current - 1))
+    setPageNumber((current) => Math.max(current - 1, 1))
   }
+
   return {
     actions,
     pageNumber,
     totalPages,
     isLoading,
+    error,
     nextPage,
     prevPage,
-    error
+    reload: loadActions, 
   }
 }
